301b88392338bf8741b04ffd857ea8103314f9a6
LEFEVRE <lefevrejulien@live.fr>
2024-07-05 13:37:29 +0200

    ok

--- src/components/Admin/Header.js
+++ src/components/Admin/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link} from 'react-router-dom'
 
 const Header = () => {
     return (
         <div className='header'>
+
+<Link to="/auth/login">Connexion</Link>
             
         </div>
     );
