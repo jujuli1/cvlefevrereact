0dd179cf93bfae155d75b11e20c1e48cf96efb8b
LEFEVRE <lefevrejulien@live.fr>
2024-07-10 10:09:26 +0200

    okdk

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
     const onSubmit = (e) =>{
         e.preventDefault()
         console.log(credential)
-        axios.post('https://cvapi.lefevre.com/', credential)
+        axios.post('https://cvlefevreapi-cccbd8eda87f.herokuapp.com/', credential)
         .then(res => {
             
             console.log(res)
